84dc3b47a8631919e1e1f24d3a3a49a4c1355e2b
sakshi2407 <lcs2021020@iiitl.ac.in>
2023-08-26 16:34:36 +0530

    added env FRONTEND_URL for cors

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ async function handleUpload(file) {
 
 const secret = process.env.JWT_SECRET;
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
